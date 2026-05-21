0bd79b2b4696ae56915e5def7d5d3dba08f0a645
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-05-21 09:47:47 -0300

    instalamos morgan y cors

--- src/server/config.js
+++ src/server/config.js
@@ -2,8 +2,11 @@ import express from "express";
 
 export default class Server {
     constructor() {
+        //invocamos a express y lo guardamos en app(prop)
         this.app = express()
+        //config el puerto = chequea en la .env sino el 3001
         this.port = process.env.PORT || 3001;
+
         
     }
 }
