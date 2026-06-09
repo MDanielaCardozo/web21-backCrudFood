7b1435f9309195db714e0146989968b197fe6a24
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-06-09 11:13:37 -0300

    Agregue el modelo de usuario y el CRUD de usuario

---
+++ src/controllers/usuarios.controllers.js
@@ -0,0 +1,50 @@
+import bcrypt from "bcrypt";
+import Usuario from "../models/usuario.js";
+
+export const crearUsuario = async (req, res) => {
+    try {
+        const saltos = bcrypt.genSaltSync(10);
+        const passwordEncriptado = bcrypt.hashSync(req.body.password, saltos);
+        req.body.password = passwordEncriptado;
+        const usuarioNuevo = new Usuario(req.body);
+        await usuarioNuevo.save();
+        res.status(201).json({ mensaje: "Usuario creado correctamente"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: "Ocurrio un error, no se pudo crear el usuario"})
+    }
+}
+
+export const listarUsuarios = async (req, res) => {
+    try {
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: "Ocurrio un error, no se pudo listar los usuarios"})
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        //verificar el email
+        const usuarioBuscado = await Usuario.findOne({ email });
+        if (!usuarioBuscado) {
+            return res.status(404).json({ mensaje: "El usuario no existe"})
+        }
+        //chequear el password
+        const passwordValido = bcrypt.compareSync(
+            password,
+            usuarioBuscado.password
+        );
+        if (!passwordValido) {
+            return res.status(401).json({ mensaje: "Contraseña incorrecta"})
+        }
+    } catch (error) {
+        console.error(error);
+        res
+        .status(500)
+        .json({ mensaje: "Ocurrio un error al intentar loguear un usuario"})
+    }
+}
