2bcfc786a80e95d302edebcbc59ac6da217aaa91
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-06-09 09:45:35 -0300

    agregue resultadoValidacion

---
+++ src/middlewares/resultadoValidacion.js
@@ -0,0 +1,12 @@
+import { validationResult } from "express-validator"
+
+const resultadoValidacion = (req, res, next) => {
+    const errores = validationResult(req)
+    //ocurrieron errores en la validacion
+    if (!errores.isEmpty()) {
+        //enviamos msj de error 400(bad request) porque falto una parte del body(producto incompleto)
+        //array de errores
+        return res.status(400).json(errores.array())
+    } 
+    next()
+}
