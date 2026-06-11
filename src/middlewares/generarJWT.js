284e598a5c02fb245947ebb1d36a7e6f678ffa13
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-06-11 10:00:09 -0300

    solucione un error de jwt sign

--- src/middlewares/generarJWT.js
+++ src/middlewares/generarJWT.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const generarJWT = (usuario, email) => {
     try {
         const payload = { usuario, email};
-        const token = jsw.sing(payload, process.env.SECRETJWT, { expiresIn: "2h" });
+        const token = jsw.sign(payload, process.env.SECRETJWT, { expiresIn: "2h" });
         return token;
     } catch (error) {
         console.error(error);
